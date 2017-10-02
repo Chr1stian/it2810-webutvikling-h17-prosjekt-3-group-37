1cf23598d003419f7b96da90b1026a47f0acebd5
Emil Schrøder <emilps@stud.ntnu.no>
2017-10-02 14:21:17 +0200

    Fixed styles in own folder

--- myapp/src/containers/App.js
+++ myapp/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './../logo.svg';
-import './../App.css';
+import './../style/App.css';
 import Appointments from './Appointments.js';
 import ReactDOM from 'react-dom';
 
--- myapp/src/containers/Appointments.js
+++ myapp/src/containers/Appointments.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './../App.css';
+import './../style/App.css';
 import Moment from 'react-moment';
 import Popup from 'react-popup';
 
