a9bfb51e213168d60c5653429bc75e2b8a07ebd8
oystpoyst <oystein.hammersland@gmail.com>
2017-10-02 14:26:14 +0200

    Added toDoList to app.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import Appointments from './Appointments.js';
 import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.openAppointments = this.openAppointments.bind(this);
+    this.openTodo = this.openTodo.bind(this);
   }
   openAppointments(){
     ReactDOM.render(<Appointments />, document.getElementById('app-area'));
+  }
+  openTodo(){
+    ReactDOM.render(<TodoList />, document.getElementById('app-area'));
   }
 
   render() {
@@ -23,7 +28,7 @@ class App extends Component {
         <p className="App-intro">
           <ul>
             <li>Notater</li>
-            <li>Todo</li>
+            <li id="todo" onClick={this.openTodo}>Todo</li>
             <li id="avtale" onClick={this.openAppointments}>Avtaler</li>
           </ul>
           <div className="App-area">
