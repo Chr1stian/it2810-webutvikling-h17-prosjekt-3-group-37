f4ea9bcffd69c3ec1de140a58628866d3a515de1
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-10-02 20:24:20 +0200

    removed unnecesary imports, added create appointment functionality

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App.js';
-import Appointments from './containers/Appointments.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
