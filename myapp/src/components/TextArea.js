7935309c5b573d388e0855541f4a6229179ecf7f
oystpoyst <oystein.hammersland@gmail.com>
2017-10-11 12:24:21 +0200

    Change added placeholder and change text in manubar

--- myapp/src/components/TextArea.js
+++ myapp/src/components/TextArea.js
@@ -31,7 +31,7 @@ handleDelete = (event) => {
         <div className="noteContainer">
           <Card className="textArea">
             <CardTitle style={{textAlign: "center"}} title="Your notes" />
-            <TextField style={{width: "500px"}} spellCheck={false} rows={13} rowsMax={13} multiLine={true} id="textarena" value= {localStorage.getItem('value', this.state.value)} onChange={this.handleChange}></TextField>
+            <TextField placeholder="Do you have anything on your mind?" style={{width: "500px"}} spellCheck={false} rows={13} rowsMax={13} multiLine={true} id="textarena" value= {localStorage.getItem('value', this.state.value)} onChange={this.handleChange}></TextField>
             <FlatButton onClick={this.handleDelete}>Delete</FlatButton>
           </Card>
         </div>
