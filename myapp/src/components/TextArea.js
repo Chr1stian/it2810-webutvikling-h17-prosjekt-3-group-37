65e6dff2f10c37d7b71f5efac3ae686ff72c537b
oystpoyst <oystein.hammersland@gmail.com>
2017-10-11 12:12:44 +0200

    Added Delete button

--- myapp/src/components/TextArea.js
+++ myapp/src/components/TextArea.js
@@ -1,5 +1,9 @@
 import React, { Component}  from 'react';
 import './../style/textarea.css';
+import Card from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import CardTitle from 'material-ui/Card/CardTitle';
+import FlatButton from 'material-ui/FlatButton';
 
 class TextArea extends Component{
   constructor(props, context){
@@ -11,19 +15,26 @@ class TextArea extends Component{
     this.handleChange = this.handleChange.bind(this);
   }
 
-handleChange(event){
+handleChange = (event) => {
   this.setState({value: event.target.value});
   localStorage.setItem('value', event.target.value);
 }
+handleDelete = (event) => {
+  localStorage.setItem('value', "");
+  this.setState({value: ""});
+
+}
+
 
     render(){
       return(
-        <div className="textArea">
-          <form>
-            <textarea id="textarena" value= {localStorage.getItem('value', this.state.value)} onChange={this.handleChange}>
-            </textarea>
-          </form>
-      </div>
+        <div className="noteContainer">
+          <Card className="textArea">
+            <CardTitle style={{textAlign: "center"}} title="Your notes" />
+            <TextField style={{width: "500px"}} spellCheck={false} rows={13} rowsMax={13} multiLine={true} id="textarena" value= {localStorage.getItem('value', this.state.value)} onChange={this.handleChange}></TextField>
+            <FlatButton onClick={this.handleDelete}>Delete</FlatButton>
+          </Card>
+        </div>
     );
   }
 }
