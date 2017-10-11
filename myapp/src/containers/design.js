1724cbc52776df0c66408e23b4f1a8c7405c3a5e
oystpoyst <oystein.hammersland@gmail.com>
2017-10-11 14:52:52 +0200

    change background color for all sites

--- myapp/src/containers/design.js
+++ myapp/src/containers/design.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -7,6 +7,7 @@ import Home from './home';
 import TodoList from './TodoList';
 import Notes from './Notes';
 import Appointments from './Appointments.js';
+import Moment from 'moment';
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -15,27 +16,77 @@ const muiTheme = getMuiTheme({
   },
 });
 
-const DesignContainer = () => (
-  <MuiThemeProvider muiTheme={muiTheme}>
-    <div>
-      <BrowserRouter >
-      <div>
-        <Navbar />
 
-        <div id="app-area">
-          <Switch>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/todo" component={TodoList} />
-            <Route path="/notes" component={Notes} />
-            <Route path="/appointment" component={Appointments} />
+class DesignContainer extends Component {
+constructor(props){
+  super();
 
-          </Switch>
+  const listWheather = ["linear-gradient(to bottom, #020111 85%,#191621 100%)",
+              "linear-gradient(to bottom, #020111 85%,#191621 100%)",
+              "linear-gradient(to bottom, #020111 60%,#20202c 100%)",
+              "linear-gradient(to bottom, #020111 10%,#3a3a52 100%)",
+              "linear-gradient(to bottom, #20202c 0%,#515175 100%)",
+              "linear-gradient(to bottom, #40405c 0%,#6f71aa 80%,#8a76ab 100%)",
+              "linear-gradient(to bottom, #4a4969 0%,#7072ab 50%,#cd82a0 100%)",
+              "linear-gradient(to bottom, #757abf 0%,#8583be 60%,#eab0d1 100%)",
+              "linear-gradient(to bottom, #82addb 0%,#ebb2b1 100%)",
+              "linear-gradient(to bottom, #94c5f8 1%,#a6e6ff 70%,#b1b5ea 100%)",
+              "linear-gradient(to bottom, #b7eaff 0%,#94dfff 100%)",
+              "linear-gradient(to bottom, #9be2fe 0%,#67d1fb 100%)",
+              "linear-gradient(to bottom, #90dffe 0%,#38a3d1 100%)",
+              "linear-gradient(to bottom, #57c1eb 0%,#246fa8 100%)",
+              "linear-gradient(to bottom, #2d91c2 0%,#1e528e 100%)",
+              "linear-gradient(to bottom, #2473ab 0%,#1e528e 70%,#5b7983 100%)",
+              "linear-gradient(to bottom, #1e528e 0%,#265889 50%,#9da671 100%)",
+              "linear-gradient(to bottom, #1e528e 0%,#728a7c 50%,#e9ce5d 100%)",
+              "linear-gradient(to bottom, #154277 0%,#576e71 30%,#e1c45e 70%,#b26339 100%)",
+              "linear-gradient(to bottom, #163C52 0%,#4F4F47 30%,#C5752D 60%,#B7490F 80%, #2F1107 100%)",
+              "linear-gradient(to bottom, #071B26 0%,#071B26 30%,#8A3B12 80%,#240E03 100%)",
+              "linear-gradient(to bottom, #010A10 30%,#59230B 80%,#2F1107 100%)",
+              "linear-gradient(to bottom, #090401 50%,#4B1D06 100%)",
+              "linear-gradient(to bottom, #090401 50%,#4B1D06 100%)"
+            ]
+  this.state = {
+    color : listWheather[parseInt(Moment().format('H'), 10)],
+    date: Moment().format('H'),
+    colorList: listWheather,
+  };
+}
+
+componentDidMount() {
+  const {colorList} = this.state;
+  this.intervalId = setInterval(() => {
+    this.setState({ date: Moment().format('H'), color: colorList[parseInt(this.state.date, 10)]});
+
+  }, 360000);
+}
+
+  render() {
+    return(
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <BrowserRouter >
+            <div>
+              <Navbar />
+
+              <div id="app-area" style= {{background: this.state.color}} >
+                <Switch>
+
+                  <Route exact path="/" component={Home} />
+                  <Route path="/todo" component={TodoList} />
+                  <Route path="/notes" component={Notes} />
+                  <Route path="/appointment" component={Appointments} />
+
+                </Switch>
+              </div>
+
+            </div>
+          </BrowserRouter>
         </div>
-      </div>
-      </BrowserRouter>
-    </div>
-  </MuiThemeProvider>
-)
+      </MuiThemeProvider>
+    );
+  };
+}
 
 export default DesignContainer;
