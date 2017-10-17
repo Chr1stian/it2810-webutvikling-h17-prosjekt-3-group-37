8c3ba078c12990b08527734f18eb8d05540125da
Emil Schrøder <emilps@stud.ntnu.no>
2017-10-17 11:10:09 +0200

    Added icons for navigationbar

--- myapp-native/src/config/router.js
+++ myapp-native/src/config/router.js
@@ -9,11 +9,23 @@ import AppointmentContainer from '../containers/AppointmentContainer';
 export const Tabs = TabNavigator({
   Note:{
     screen: NoteContainer,
+    navigationOptions: {
+      tabBarLaber: 'Note',
+      tabBarIcon: ({ tintColor }) => <Icon name="note" size={35} color={tintColor}/>
+    },
   },
   Todo:{
     screen: TodoContainer,
+    navigationOptions: {
+      tabBarLaber: 'Todo',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor}/>
+    },
   },
   Appointments:{
     screen: AppointmentContainer,
+    navigationOptions: {
+      tabBarLaber: 'Appointments',
+      tabBarIcon: ({ tintColor }) => <Icon name="event" size={35} color={tintColor}/>
+    },
   }
 });
