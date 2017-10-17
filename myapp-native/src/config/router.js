76ad4df9ddfd83101fc4a6eb7d0881d4712b07eb
Emil Schrøder <emilps@stud.ntnu.no>
2017-10-17 10:47:10 +0200

    Setup for navigation

---
+++ myapp-native/src/config/router.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import NoteContainer from '../containers/NoteContainer';
+import TodoContainer from '../containers/TodoContainer';
