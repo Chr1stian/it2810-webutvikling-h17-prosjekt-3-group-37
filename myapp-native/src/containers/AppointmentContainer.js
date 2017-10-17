9a49dcaa69d165cd5380ad4f3d596930ca9c97bc
oystpoyst <oystein.hammersland@gmail.com>
2017-10-17 18:55:56 +0200

    Styled buttons and fixed such that input text clears when submitting

--- myapp-native/src/containers/AppointmentContainer.js
+++ myapp-native/src/containers/AppointmentContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, TextInput, AsyncStorage, ScrollView } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import { Button } from 'react-native-elements';
 import uuid from 'uuid';
 import Moment from 'moment';
+import AppointmentListItem from './../components/AppointmentListItem';
+
 
 
 
@@ -40,6 +42,10 @@ export default class AppointmentContainer extends React.Component {
     AsyncStorage.setItem('appointments',JSON.stringify(changedList));
   }
 */
+componentDidMount(){
+  this.removeOldAppointments();
+
+}
 
   addAppointment = () => {
     let {title} = this.state;
@@ -63,10 +69,35 @@ export default class AppointmentContainer extends React.Component {
         //Saves the list to localStorage
         //Reloads form to refresh the input fields
         console.log(appointmentList);
+        this.setState({
+          title:"",
+          date:"",
+          sTime:"",
+          eTime:"",
+          place:""
+
+      });
+
       }
     }else{
       alert("All fields must be filled");
     }
+  }
+
+  deleteAppointment = (appointment) => {
+    let {appointmentList} = this.state;
+    let i = appointmentList.indexOf(appointment);
+    appointmentList.splice(i,1);
+    //Sets the state and localStorage to the new list without the deleted appointment
+    this.setState({appointmentList: appointmentList});
+  }
+
+  removeOldAppointments = () => {
+    let {appointmentList} = this.state;
+    let today = new Date();
+    //Creates a new list with appointments with a date today or later
+    let changedList = appointmentList.filter(function (appointment) {return (appointment.date.split("/").join("-")) >= (Moment(today).format("DD/MM/YYYY")).split("/").join("-")});
+    this.setState({appointmentList: changedList});
   }
 
   render() {
@@ -75,6 +106,8 @@ export default class AppointmentContainer extends React.Component {
       <View style={styles.container}>
         <Text>Create Appointment</Text>
         <TextInput
+          value= {this.state.title}
+
           style={{width: 150}}
           placeholder="Enter title"
           onChangeText={(title) => {
@@ -100,7 +133,7 @@ export default class AppointmentContainer extends React.Component {
           format="HH:mm"
         placeholder="Enter end time"/>
         <TextInput
-
+          value= {this.state.place}
           style={{width: 150}}
           placeholder="enter place/address"
           onChangeText={(place) => {
@@ -108,7 +141,16 @@ export default class AppointmentContainer extends React.Component {
         <Button
           onPress={this.addAppointment}
           style={{width: 150}}
-        value="Add"/>
+          title="Add"
+          backgroundColor="#841584"
+        />
+        <ScrollView>
+          <Text> Your appointments</Text>
+
+          { appointmentList.map((item) => <AppointmentListItem appointment={item} key={item.ID} deleteAppointment={this.deleteAppointment}/>) }
+        </ScrollView>
+
+
       </View>
     );
   }
